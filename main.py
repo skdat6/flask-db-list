07251ef3e4ef546ed49f29b8bfd00cec8468c6e5
skdat6 <dobretheo@gmail.com>
2022-09-09 15:32:14 +0300

    added DB model, configured new page route, configured html

--- main.py
+++ main.py
@@ -10,15 +10,38 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') or \
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(120), unique=False, nullable=False)
+    rating = db.Column(db.Float, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+db.create_all()
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    return redirect(url_for('home'))
+    if request.method == "POST":
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"])
+        db.session.add(new_book)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
