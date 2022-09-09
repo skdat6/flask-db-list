fb0b9c76b551593ae1a92dbdc352f273f2a9749c
skdat6 <dobretheo@gmail.com>
2022-09-09 15:17:54 +0300

    added main html file

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') or 'try-it-i-guess'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') or \
+                          'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
