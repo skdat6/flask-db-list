01dd86cda538a03fe68504c37b6a504d127f5e0a
skdat6 <dobretheo@gmail.com>
2022-09-28 17:23:26 +0300

    added login page, some functionality and styling - close #8 added register page, some functionality and styling - close #9 added navigation bar on base html and extended functionality on other pages, added some styling for testing- close #10
    added routes for /login /register in order to test implementations

--- app/main.py
+++ app/main.py
@@ -69,6 +69,20 @@ def delete():
 
     return redirect(url_for('home'))
 
+@app.route("/register")
+def register():
+
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
